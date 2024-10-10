0115296a0ad807291dfe8ef08ed18ee1b9f18807
robertkkan <RobertKan2020@gmail.com>
2024-10-09 23:17:16 -0500

    new folders

---
+++ components/ui/button.tsx
@@ -0,0 +1,50 @@
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center whitespace-nowrap rounded-[4px] border-[0.8px] px-[1rem] py-[0.4375rem] uppercase transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none b_mono",
+  {
+    variants: {
+      variant: {
+        default:
+          "text-secondary-color border-tertiary-color hover:bg-fill-color",
+        selected:
+          "text-primary-color border-primary-color bg-fill-color",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+)
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+  isSelected?: boolean
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, isSelected, asChild = false, onClick, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button"
+    return (
+      <Comp
+        className={cn(buttonVariants({ 
+          variant: isSelected ? "selected" : variant, 
+          className 
+        }))}
+        ref={ref}
+        onClick={onClick}
+        {...props}
+      />
+    )
+  }
+)
+Button.displayName = "Button"
+
+export { Button, buttonVariants }
